d367618516320e1d16ba5d867a6109418c2334ab
Josh Goldberg <joshuakgoldberg@outlook.com>
2016-05-27 19:35:31 -0700

    Added node<6 handling to src\main.ts (#57)
    Also changed newer JS concepts (such as `() =>{...}`) to older, for
    really old node versions.

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,24 @@
+/* tslint:disable */
 "use strict";
 import { Server } from "./server/server";
 
-/**
- * Path to a settings file storing server settings.
- */
-const settingsFileName: string = "assassins.json";
+(function () {
+    if (parseInt(process.version.match(/[0-9]/)[0]) < 6) {
+        console.log("Node >=6.0.0 required. You're on '" + process.version + "'.");
+        console.log("https://nodejs.org/en/download");
+        return;
+    }
 
-Server.createFromFile(settingsFileName)
-    .then((server: Server): void => server.run())
-    .catch(error => console.error(`${error}\n:(`));
+    /**
+     * Path to a settings file storing server settings.
+     */
+    var settingsFileName: string = "assassins.json";
+
+    Server.createFromFile(settingsFileName)
+        .then(function (server: Server): void {
+            server.run();
+        })
+        .catch(function (error) {
+            console.error(error, "\n:(");
+        });
+})();

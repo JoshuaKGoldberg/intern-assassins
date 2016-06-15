fc6e5379c2abaf9de907e00fed6f00ad887c772a
Julius Liu <juliusl@microsoft.com>
2016-06-15 10:31:46 -0700

    - So my interpretation of this issue was the explicit ability to cancel an action. You can do this two ways with this change. Pressing the action button again, or pressing the cancel button. For now I didn't make it an X just because it looks like more UI work should be done around what an action button should actually look like, so for now some text should be suffecient. (#8)

--- src/site/scripts/components/profile/actionbutton.tsx
+++ src/site/scripts/components/profile/actionbutton.tsx
@@ -130,11 +130,29 @@ export class ActionButton extends React.Component<IActionButtonProps, IActionBut
         }
 
         return (
+            <div className="action-confirmation">
             <input
-                className="action-confirmation"
                 onClick={onClick}
                 type="button"
-                value={value} />);
+                value={value} />
+                {this.renderCancel()}
+            </div>);
+    }
+
+    private renderCancel(): JSX.Element {
+        let onClick: () => void;
+
+        if (this.state.expanded) {
+            onClick = (): void => {
+                this.toggleExpansion();
+            };
+        }
+
+        return (
+          <input
+            onClick={onClick}
+            type="button"
+            value="Cancel"/>);
     }
 
     /**
@@ -144,7 +162,8 @@ export class ActionButton extends React.Component<IActionButtonProps, IActionBut
         if (this.state.expanded) {
             this.setState({
                 confirmation: this.state.confirmation,
-                expanded: false
+                expanded: false,
+                delay: 0
             });
 
             return;

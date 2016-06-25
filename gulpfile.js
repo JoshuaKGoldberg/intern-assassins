938ce4a10c583d0157f7aca9e201f314bfd00ef3
Josh Goldberg <josh@fullscreenmario.com>
2016-06-24 22:19:16 -0700

    Added gulp-uglify for uglification
    Fixes #124.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const babelify = require("babelify");
 const browserify = require("browserify");
+const buffer = require("vinyl-buffer");
 const fs = require("fs");
 const gulp = require("gulp");
 const cucumber = require("gulp-cucumber");
@@ -9,6 +10,7 @@ const runSequence = require("run-sequence");
 const source = require("vinyl-source-stream");
 const ts = require("gulp-typescript");
 const tslint = require("gulp-tslint");
+const uglify = require("gulp-uglify");
 
 gulp.task("browserify", () => {
     const browsering = browserify(
@@ -22,6 +24,8 @@ gulp.task("browserify", () => {
         .transform("babelify", { presets: ["es2015"] })
         .bundle()
         .pipe(source("main.js"))
+        .pipe(buffer())
+        .pipe(uglify())
         .pipe(gulp.dest("src/site/scripts/bundled"));
 });
 

fd16a83033063e9e3e030793108930a7605675f9
Josh Goldberg <josh@fullscreenmario.com>
2016-06-28 22:41:47 -0700

    Removed requirejs configs from index.ts
    This stuff isn't being used now that browserify is bundling everything.

--- src/site/scripts/index.ts
+++ src/site/scripts/index.ts
@@ -1,13 +1,3 @@
 declare var requirejs: any;
 
-requirejs.config({
-    paths: {
-        "main": "bundled/main",
-        "jszip": "../../../node_modules/xlsx/dist/jszip",
-        "react": "../../../node_modules/react/dist/react",
-        "react-dom": "../../../node_modules/react-dom/dist/react-dom",
-        "xlsx": "../../../node_modules/xlsx/dist/xlsx.core.min"
-    }
-});
-
-requirejs(["main"]);
+requirejs(["bundled/main"]);

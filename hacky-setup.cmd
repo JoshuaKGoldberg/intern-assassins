f79bbcc47567e8f331a17de7a9aaa5421a002917
Josh Goldberg <josh@fullscreenmario.com>
2016-06-28 23:54:41 -0700

    Removed hacky setup script

--- hacky-setup.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-
-REM react
-move .\node_modules\react\dist\react.js .\node_modules\react\dist\react-save.js 
-copy .\node_modules\react\dist\react.min.js .\node_modules\react\dist\react.js
-
-REM react-dom
-move .\node_modules\react-dom\dist\react-dom.js .\node_modules\react-dom\dist\react-dom-save.js 
-copy .\node_modules\react-dom\dist\react-dom.min.js .\node_modules\react-dom\dist\react-dom.js 
